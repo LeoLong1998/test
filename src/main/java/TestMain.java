9908fd834d1466498b571223c60cee40cf27f120
longxin <longxin@bingosoft.net>
2020-06-07 20:05:02 +0800

    frist try

---
+++ src/main/java/TestMain.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+/**
+ * @author long
+ * @date 2020/6/1 - 9:39
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
